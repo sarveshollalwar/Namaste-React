f1a6d7630433611f8bdeac03f0aae3e6b1cff3ac
sarvesh ollalwar <sarveshollalwar08@gmail.com>
2023-09-11 12:34:44 +0530

    The glaze

---
+++ App.js
@@ -0,0 +1,5 @@
+
+    const element = React.createElement("h1",{},"Namaste everyone")
+    console.log(element)
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+    root.render(element);
